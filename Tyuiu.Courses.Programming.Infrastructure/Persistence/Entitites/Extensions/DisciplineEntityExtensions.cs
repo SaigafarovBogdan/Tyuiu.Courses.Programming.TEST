d284774f1cd3ef15aa78473d29e0a7ac2df1f293
SaigafarovBogdan <sajgafarovb@gmail.com>
2026-04-11 08:45:58 +0500

    feat(enitties): added entity extensions, discipline extension

---
+++ Tyuiu.Courses.Programming.Infrastructure/Persistence/Entitites/Extensions/DisciplineEntityExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.Courses.Programming.Infrastructure.Persistence.Entitites.Extensions
+{
+	public static class DisciplineEntityExtensions
+	{
+		public static bool HasCourses(this DisciplineEntity discipline)
+		{
+			return discipline?.Courses?.Any() ?? false;
+		}
+	}
+}
