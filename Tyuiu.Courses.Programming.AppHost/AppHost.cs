741a8e86e37af32cc9b4a679043bb10d3da18489
SaigafarovBogdan <sajgafarovb@gmail.com>
2026-05-05 20:08:50 +0500

    refactor(host): database data is moved to Secrets

--- Tyuiu.Courses.Programming.AppHost/AppHost.cs
+++ Tyuiu.Courses.Programming.AppHost/AppHost.cs
@@ -1,10 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var pgUser = builder.AddParameter("postgresUser", secret: true);
+var pgPassword = builder.AddParameter("postgresPassword", secret: true);
+
 var postgres = builder.AddPostgres("postgres")
 	.WithImage("postgres:17.7-alpine")
 	.WithContainerName("database.server")
-	.WithEnvironment("POSTGRES_USER", "postgres")
-	.WithEnvironment("POSTGRES_PASSWORD", "97GCKyZi39xJ")
+	.WithEnvironment("POSTGRES_USER", pgUser)
+	.WithEnvironment("POSTGRES_PASSWORD", pgPassword)
 	.WithEnvironment("POSTGRES_DB", "aspnetappdb")
 	.WithVolume("postgres-data", "/var/lib/postgresql/data")
 	.WithEndpoint(port: 5432, targetPort: 5432, name: "postgres")
